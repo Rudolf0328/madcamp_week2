98faa7d5b5cc26d9ea4ae0c17c731698d71d9ba3
rudolf0328 <ck0716080@gmail.com>
2022-01-07 02:06:16 +0900

    tab3, tab4

---
+++ app/src/main/java/com/example/madcamp_week2/ui/chatroom/AddChatRoomActivity.java
@@ -0,0 +1,16 @@
+package com.example.madcamp_week2.ui.chatroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madcamp_week2.R;
+
+public class AddChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_chat_room);
+    }
+}
